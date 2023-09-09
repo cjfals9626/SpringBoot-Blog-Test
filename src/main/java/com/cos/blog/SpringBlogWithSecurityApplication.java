e2e7c9aafdd776aa4a5368d35a2e0ba6b1417fde
cjfals9626 <cjfals9626@naver.com>
2023-09-09 20:13:07 +0900

    환경세팅 완료

---
+++ src/main/java/com/cos/blog/SpringBlogWithSecurityApplication.java
@@ -0,0 +1,13 @@
+package com.cos.blog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBlogWithSecurityApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBlogWithSecurityApplication.class, args);
+    }
+
+}
